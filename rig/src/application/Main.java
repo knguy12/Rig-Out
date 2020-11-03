7854f2ff244f239a2a5b76995b87f18815e69bda
Kevin Nguyen <nguyenkevin7326@gmail.com>
2020-11-03 14:13:05 -0800

    -Added in method to get Weather data

--- rig/src/application/Main.java
+++ rig/src/application/Main.java
@@ -10,12 +10,12 @@ import javafx.fxml.FXMLLoader;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			String test = "Test";
-			
+		try {			
 			VBox root = (VBox)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
--- rig/src/application/weatherChecker.java
+++ rig/src/application/weatherChecker.java
@@ -1,5 +1,38 @@
 package application;
 
-public class weatherChecker {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
+public class weatherChecker {
+	public String getTemp() throws IOException {
+//		String APIkey = "6089342a45d76e01e12c36bd94042e73";
+//		String location = "San Jose";
+		String urlString = "http://api.openweathermap.org/data/2.5/weather?q=San%20Jose&appid=6089342a45d76e01e12c36bd94042e73&units=imperial";
+		StringBuilder result = new StringBuilder();
+		try {
+			URL url = new URL(urlString);
+			URLConnection conn = url.openConnection();
+			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String output;
+			
+			while((output = rd.readLine()) != null){
+				result.append(output);
+			}
+			rd.close();
+			System.out.print(result);
+		} 
+		catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		return result.toString();
+	}
+	public static void main(String[] args) throws IOException {
+		weatherChecker check = new weatherChecker();
+		check.getTemp();
+	}
 }
