bdaf8d1443032da55805a005461b617c74262ad2
Kevin Nguyen <nguyenkevin7326@gmail.com>
2020-11-10 16:23:40 -0800

    apparelTypes Class Update

--- rig/src/application/apparelTypes.java
+++ rig/src/application/apparelTypes.java
@@ -1,5 +1,9 @@
 package application;
 
 public class apparelTypes {
-
+	private String[] types;
+	public apparelTypes() {
+		types = new String[] {"Hat", "Shirt", "Pants", "Shoes", "Jacket"};
+	}
+	
 }
