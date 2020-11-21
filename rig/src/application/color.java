d7877dc314fbfe2f06a5774c928dc83d3427b1a6
Addison Fung <addisonfung11@gmail.com>
2020-11-20 20:44:24 -0800

    Added cold for weatherChecker and the colors in color.java

--- rig/src/application/color.java
+++ rig/src/application/color.java
@@ -1,5 +1,10 @@
 package application;
 
 public class color {
+	private String[] colors;
 
+	public color() {
+		colors = new String[] { "Red", "Blue", "Green", "Yellow", "Brown", "Black", "White", "Grey", "Orange", "Purple",
+				"Indigo" };
+	}
 }
--- rig/src/application/weatherChecker.java
+++ rig/src/application/weatherChecker.java
@@ -9,42 +9,49 @@ import java.net.URLConnection;
 
 public class weatherChecker {
 	public String getTemp() throws IOException {
-	
+
 		String urlString = "http://api.openweathermap.org/data/2.5/weather?q=San%20Jose&appid=6089342a45d76e01e12c36bd94042e73&units=imperial";
 		StringBuilder result = new StringBuilder();
-		
-		//reads the data from weather API
+
+		// reads the data from weather API
 		try {
 			URL url = new URL(urlString);
 			URLConnection conn = url.openConnection();
 			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 			String output;
-			
-			while((output = rd.readLine()) != null){
+
+			while ((output = rd.readLine()) != null) {
 				result.append(output);
 			}
-	
+
 			rd.close();
-		} 
-		catch (MalformedURLException e) {
+		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
-		//Parses the JSON data and finds the temperature value
+		// Parses the JSON data and finds the temperature value
 		String temp = result.toString();
 		String[] totalJSON = temp.split("\\{");
-		temp = totalJSON[4].substring(7,12);
+		temp = totalJSON[4].substring(7, 12);
 		return temp;
 	}
-	
+
 	public boolean isHot() throws NumberFormatException, IOException {
-		double temperature  = Double.parseDouble(getTemp());
-		if(temperature < 80)
+		double temperature = Double.parseDouble(getTemp());
+		if (temperature < 80)
 			return false;
 		return true;
 	}
+
+	public boolean isCold() throws NumberFormatException, IOException {
+		double temperature = Double.parseDouble(getTemp());
+		if (temperature > 50)
+			return false;
+		return true;
+	}
+
 	public static void main(String[] args) throws IOException {
 		weatherChecker check = new weatherChecker();
 		String temperature = check.getTemp();
-		System.out.print(temperature);
+		System.out.print(temperature + " " + "degrees fahrenheit");
 	}
 }
